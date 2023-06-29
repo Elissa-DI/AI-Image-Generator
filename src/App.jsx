11d5bd2db2e4193eca97c650fd2e6045fd2f4067
Elissa <elissadusabe@gmail.com>
2023-06-29 17:17:45 +0200

    Initialized a project

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <h1 className='text-3xl font-bold underline text-yellow-800'>        
+      Bull is gonna do it
+      </h1>
+    </>
+  )
+}
+
+export default App
