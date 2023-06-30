14319354cddcfe0598acf2d3cd4c834b869afb04
Elissa <elissadusabe@gmail.com>
2023-06-30 21:31:18 +0200

    new features

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -48,7 +48,19 @@ const Home = () => {
                 )
               }        
               <div className='grid lg:grid-cols-4 sm:grid-cols-3 xs:grid-cols-2 grid-cols-1 gap-3'>
-                                              
+                {
+                  searchText ? (
+                    <RenderCards 
+                      data={[]}
+                      title="No search results found"                
+                    />
+                  ) : (
+                    <RenderCards 
+                      data={[]}
+                      title="No posts found"                
+                    />
+                  )
+                }                                              
               </div>  
             </>
           )
