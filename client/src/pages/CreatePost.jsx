d17b5ad5f7596f814327bac4c3485936b20269ac
Elissa <elissadusabe@gmail.com>
2023-07-01 10:04:00 +0200

    new features

--- client/src/pages/CreatePost.jsx
+++ client/src/pages/CreatePost.jsx
@@ -6,6 +6,14 @@ import { preview } from '../assets';
 import { getRandomPrompt } from '../utils';
 import { FormField, Loader } from '../components';
 
+const handleSubmit = () => {
+
+}
+
+const handleChange = (e) => {
+
+}
+
 const CreatePost = () => {
   const navigate = useNavigate();
   const [form, setForm] = useState({
@@ -20,6 +28,43 @@ const CreatePost = () => {
     <section
       className='max-w-7xl mx-auto'
     >
+      <div>
+        <h1
+         className='font-extrabold text-[#222328] text-[32px]' 
+        >Create</h1>
+        <p
+         className='mt-2 text-[#666e75] text-[14px] max-w-[500px]'
+        >
+          Create imaginative and visually stunning images through DALL-E AI and share them with the community
+        </p>
+      </div>
+      <form
+        className='mt-16 max-w-3xl'
+        onSubmit={handleSubmit}
+      >
+        <div 
+          className='flex flex-col gap-5'
+        >
+          <FormField
+            labelName="Your name"
+            type="text"
+            name="name"
+            placeholder="John Doe"
+            value={form.name}
+            handleChange={handleChange}
+          />
+
+          <FormField
+            labelName="Prompt"
+            type="text"
+            name="prompt"
+            placeholder="John Doe"
+            value={form.name}
+            handleChange={handleChange}
+          />
+
+        </div>
+      </form>
       
     </section>
   )
