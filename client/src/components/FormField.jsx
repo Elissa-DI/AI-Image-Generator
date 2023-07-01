d971c1dccf3464637ec0129c1fc58554e939a1e7
Elissa <elissadusabe@gmail.com>
2023-07-01 10:13:34 +0200

    new features

--- client/src/components/FormField.jsx
+++ client/src/components/FormField.jsx
@@ -1,12 +1,18 @@
 /* eslint-disable no-unused-vars */
 import React from 'react'
 
-const FormField = () => {
+const FormField = (
+  { LabelName, type, name, placeholder, value, handleChange, isSurprisseMe, handleSurpriseMe }
+) => {
   return (
     <div>
-      <div
-        className='flex items-center gap-2 mb-2'
-      >
+      <div className='flex items-center gap-2 mb-2'>
+        <label
+          htmlFor={name}
+          className='block text font-medium text-gray-900'
+        >
+
+        </label>
 
       </div>
     </div>
