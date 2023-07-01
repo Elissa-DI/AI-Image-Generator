a488432831c2e25f93f8614a901cd60d4d5906e7
Elissa <elissadusabe@gmail.com>
2023-07-01 09:50:34 +0200

    new features

--- client/src/assets/index.js
+++ client/src/assets/index.js
@@ -1,2 +1,9 @@
-import logo from "./logo.svg" 
-export default logo
+import download from './download.png';
+import logo from './logo.svg';
+import preview from './preview.png';
+
+export {
+  download,
+  logo,
+  preview,
+};
--- client/src/pages/CreatePost.jsx
+++ client/src/pages/CreatePost.jsx
@@ -4,11 +4,24 @@ import { useNavigate } from 'react-router-dom'
 
 import { preview } from '../assets';
 import { getRandomPrompt } from '../utils';
-import { FormField } from '../components';
+import { FormField, Loader } from '../components';
 
 const CreatePost = () => {
+  const navigate = useNavigate();
+  const [form, setForm] = useState({
+    name: '',
+    prompt: '',
+    photo: '',
+  })
+  const [generatingImg, setgeneratingImg] = useState(false);
+  const [loading, setLoading] = useState(false);
+
   return (
-    <div>CreatePost</div>
+    <section
+      className='max-w-7xl mx-auto'
+    >
+      
+    </section>
   )
 }
 
