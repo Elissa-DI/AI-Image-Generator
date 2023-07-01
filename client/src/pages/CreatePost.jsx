bacd57acf8c2879bce40f732e5478e62c14042eb
Elissa <elissadusabe@gmail.com>
2023-07-01 16:26:46 +0200

    new features

--- client/src/pages/CreatePost.jsx
+++ client/src/pages/CreatePost.jsx
@@ -104,8 +104,24 @@ const CreatePost = () => {
           <button
             type='button'
             onClick={generateImage}
+            className='text-white bg-green-700 font-medium rounded-md text-sm w-full sm:w-auto px-5 py-2.5 text-center'
           >
-            {generateImage ? 'Generating...' : 'Generate'}
+            {generatingImg ? 'Generating...' : 'Generate'}
+          </button>
+        </div>
+
+        <div
+          className='mt-10'
+        >
+          <p
+            className='mt-2 text-[#666e75] text-[14px]'
+          >Once you have created an imageyou want , you can share it with others in the community
+          </p>
+          <button 
+            type='submit'
+            className='mt-3 text-white bg-[#6460ff] font-medium rounded-md text-sm w-full sm:w-auto px-5 py-2.5 text-center'
+          >
+            {loading ? 'Sharing...' : 'Share with the community'}
           </button>
         </div>
       </form>
