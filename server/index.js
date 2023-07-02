e82328ef451482edacce765ebb5c1333352eb401
Elissa <elissadusabe@gmail.com>
2023-07-02 11:53:38 +0200

    new features

--- server/index.js
+++ server/index.js
@@ -2,6 +2,10 @@ import express from 'express'
 import * as dotenv from 'dotenv'
 import cors from 'cors'
 
+import connectDB from './mongodb/connect.js'
+import postRoutes from './routes/postRoutes.js'
+import dalleRoutes from './routes/dalleRoutes.js'
+
 dotenv.config();
 
 const app = express();
@@ -9,10 +13,20 @@ const port = 8200;
 app.use(cors());
 app.use(express.json({ limit: '50mb' }));
 
+app.use('/api/v1/post', postRoutes)
+app.use('/api/v1/dalle', dalleRoutes)
+
 app.get('/', async (req, res) => {
     res.send('Hello Elissa!')
 })
 
-app.listen(port, () => {
-    console.log(`Server started on port htt://localhost:${port}`);
-})
+const startServer = async () => {    
+    try {
+        connectDB(process.env.MONGODB_URL);
+        app.listen(port, () => console.log(`Server started on port htt://localhost:${port}`))
+    } catch (error) {
+        console.log(err.message);        
+    }
+}
+
+startServer();
