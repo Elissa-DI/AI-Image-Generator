5a9e580969da473acb796adc1ebd891fac744a99
Elissa <elissadusabe@gmail.com>
2023-07-04 14:17:28 +0200

    new features

--- client/src/pages/CreatePost.jsx
+++ client/src/pages/CreatePost.jsx
@@ -1,4 +1,5 @@
-/* eslint-disable no-undef */
+/* eslint-disable react/prop-types */
+/* eslint-disable no-unused-vars */
 
 import React, { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
@@ -30,7 +31,7 @@ const CreatePost = () => {
     if (form.prompt) {
       try {
         setGeneratingImg(true);
-        const response = await fetch('http://localhost:8200/api/v1/dalle', {
+        const response = await fetch('https://dalle-arbb.onrender.com/api/v1/dalle', {
           method: 'POST',
           headers: {
             'Content-Type': 'application/json',
@@ -39,20 +40,18 @@ const CreatePost = () => {
             prompt: form.prompt,
           }),
         });
-        const data = await response.text();
-        console.log(data); // Log the response data
-  
-        // Continue with the rest of your code...
+
+        const data = await response.json();
+        setForm({ ...form, photo: `data:image/jpeg;base64,${data.photo}` });
       } catch (err) {
         alert(err);
       } finally {
         setGeneratingImg(false);
       }
     } else {
-      alert('Please provide a proper prompt');
+      alert('Please provide proper prompt');
     }
   };
-  
 
   const handleSubmit = async (e) => {
     e.preventDefault();
@@ -60,7 +59,7 @@ const CreatePost = () => {
     if (form.prompt && form.photo) {
       setLoading(true);
       try {
-        const response = await fetch('https://localhost:8200/api/v1/post', {
+        const response = await fetch('https://dalle-arbb.onrender.com/api/v1/post', {
           method: 'POST',
           headers: {
             'Content-Type': 'application/json',
