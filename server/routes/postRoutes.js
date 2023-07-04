2c3c13ac95a55666319a673b1e54ecf9b230e8e8
Elissa <elissadusabe@gmail.com>
2023-07-04 14:04:07 +0200

    new features

--- server/routes/postRoutes.js
+++ server/routes/postRoutes.js
@@ -15,7 +15,12 @@ cloudinary.config({
 });
 
 router.route('/').get(async (req, res) => {
-
+  try {
+    const posts = await Post.find({});
+    res.status(200).json({ success: true, data: posts });
+  } catch (err) {
+    res.status(500).json({ success: false, message: 'Fetching posts failed, please try again' });
+  }
 });
 
 router.route('/').post(async (req, res) => {
